3f8901c48c9554b144ccb48d3c064b6de83d8eab
Samuel Jonathan JOSEPH <140217949+josephsamijona@users.noreply.github.com>
2023-10-17 14:46:52 -0700

    change made

--- src/buskogear/blogin.py
+++ src/buskogear/blogin.py
@@ -1,17 +1,14 @@
- 
-import customtkinter
-import os
-import PIL
-
 import tkinter as tk
+import sqlite3
 import customtkinter
 import os
 from PIL import ImageTk, Image
-import sqlite3
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("green")
+# Initialiser la configuration de customtkinter
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
+# Créer une classe personnalisée pour la fenêtre de connexion
 class LoginWindow(customtkinter.CTk):
 
     def __init__(self):
@@ -21,20 +18,25 @@ class LoginWindow(customtkinter.CTk):
         self.resizable(0, 0)
         self.state('zoomed')
 
+        # Créer une étiquette avec une image de fond
         img1 = ImageTk.PhotoImage(Image.open("./assets/6881.jpg"))
         self.l1 = customtkinter.CTkLabel(master=self, image=img1)
         self.l1.pack()
 
+        # Créer un cadre pour le formulaire de connexion
         self.frame = customtkinter.CTkFrame(master=self.l1, width=520, height=560, corner_radius=0)
         self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
+        # Charger les images pour le mode clair et sombre
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets")
 
         self.labo = customtkinter.CTkImage(Image.open(os.path.join(image_path, "0.png")), size=(125, 100))
 
+        # Placer l'image sur le cadre
         self.home_labo = customtkinter.CTkLabel(master=self.frame, text="", image=self.labo)
         self.home_labo.place(x=185, y=120)
 
+        # Créer des étiquettes et des champs pour le nom d'utilisateur et le mot de passe
         self.l2 = customtkinter.CTkLabel(master=self.frame, text="BIENVENUE", font=('Century Gothic', 15))
         self.l2.place(x=205, y=45)
         self.l3 = customtkinter.CTkLabel(master=self.frame, text="CONNECTEZ-VOUS", font=('Century Gothic', 10))
@@ -44,44 +46,39 @@ class LoginWindow(customtkinter.CTk):
         self.entry2 = customtkinter.CTkEntry(master=self.frame, width=220, placeholder_text='mot de passe', show="*")
         self.entry2.place(x=150, y=325)
 
+        # Créer un bouton de connexion
         self.button1 = customtkinter.CTkButton(master=self.frame, width=95, text="connexion", command=self.login)
         self.button1.place(x=205, y=400)
 
+    # Définir la fonction de connexion
     def login(self):
-        # Get the username and password from the entries
+        # Récupérer le nom d'utilisateur et le mot de passe depuis les champs
         username = self.entry1.get()
         password = self.entry2.get()
 
-        # Connexion à la base de données SQLite
-        conn = sqlite3.connect("base_de_donnees.db")
+        # Se connecter à la base de données SQLite
+        conn = sqlite3.connect("clinique.db")
         cursor = conn.cursor()
 
-        # Vérification des informations de connexion dans la base de données
-        cursor.execute("SELECT * FROM user WHERE username=? AND password=?", (username, password))
+        # Interroger la base de données pour vérifier si le nom d'utilisateur et le mot de passe sont valides
+        cursor.execute("SELECT * FROM Utilisateurs WHERE Username=? AND Password=?", (username, password))
         user = cursor.fetchone()
 
+        conn.close()
+
         if user:
-            # L'utilisateur est authentifié avec succès
-            if user[-1] == 'admin':
-                # Si c'est un administrateur, faire quelque chose (par exemple, ouvrir une fenêtre d'administration)
-                pass
-            elif user[-1] == 'doctor':
-                # Si c'est un médecin, faire quelque chose (par exemple, ouvrir une fenêtre de médecin)
-                pass
-            elif user[-1] == 'nurse':
-                # Si c'est une infirmière, faire quelque chose (par exemple, ouvrir une fenêtre d'infirmière)
-                pass
-            else:
-                # Si c'est un patient, faire quelque chose (par exemple, ouvrir une fenêtre de patient)
-                pass
+            # Si le nom d'utilisateur et le mot de passe sont valides, créer une nouvelle fenêtre pour la page de bienvenue
+            w = customtkinter.CTk()
+            w.geometry("1280x720")
+            w.title("Welcome")
+            l1 = customtkinter.CTkLabel(master=w, text="Home Page", font=('Century Gothic', 60))
+            l1.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
         else:
-            # Afficher un message d'erreur pour une connexion invalide
+            # Afficher un message d'erreur en cas de connexion invalide
             error_label = customtkinter.CTkLabel(master=self.frame, text="Nom d'utilisateur ou mot de passe invalide", font=('Century Gothic', 12), fg_color='red')
             error_label.place(x=200, y=430)
 
-        # Fermer la connexion à la base de données
-        conn.close()
-
+# Si vous souhaitez exécuter la fenêtre de connexion en tant qu'application autonome
 if __name__ == "__main__":
     login_window = LoginWindow()
     login_window.mainloop()
